2a686e5d6d9364f3fee7f2238973d657412b7aba
Varsha0011 <144754861+Varsha0011@users.noreply.github.com>
2024-06-05 05:08:22 +0000

    Conversion of Data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score=true
+
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33"=> 33
+// "33abc"=> NaN
+//true=>1 false=>0
+
+let isLoggedIn=0
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+ //So, the conclusion is that 
+ //1=> true 0=> false " "=>false "Varsha"=> false
+ let value=33
+ let intToString=String(value)
+ console.log(typeof intToString);
